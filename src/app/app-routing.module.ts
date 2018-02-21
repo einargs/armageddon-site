fc4aa805a21d0eca4222b8ec10fe807a4f3449f1
einargs <einargs@gmail.com>
2018-02-21 16:37:21 -0600

    Removed old hacks.
    Clean up to start working on the full structure.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ControlPageComponent } from "./control-page/control-page.component";
-
 const routes: Routes = [
-  { path:"", component: ControlPageComponent }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { SocketIoModule } from "ng-socket-io";
 
 import { SharedModule } from "./shared/shared.module";
 import { AppRoutingModule } from './app-routing.module';
-import { ArmControllerService } from "./arm-controller.service";
 import { AppComponent } from './app.component';
-import { ControlPageComponent } from "./control-page/control-page.component";
 
 import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ControlPageComponent
+    AppComponent
   ],
   imports: [
     SharedModule,
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    SocketIoModule.forRoot({
-      url: "http://localhost:3000"
-    }),
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [ArmControllerService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
