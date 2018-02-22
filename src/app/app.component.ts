cea132bd849bc526659ea42487affb1cdb61b939
einargs <einargs@gmail.com>
2018-02-21 18:15:59 -0600

    Belatedly removed an orphan import.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
-import { ArmControllerService } from "./arm-controller.service";
-
 @Component({
   selector: 'arm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+
 }
