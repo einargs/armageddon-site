7379eacdf338e22dbfa1d339f5b749a1ab84378f
einargs <einargs@gmail.com>
2018-03-01 22:22:27 -0600

    Added NgRx dev-tools.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,6 +6,7 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { StoreModule } from "@ngrx/store";
 import { EffectsModule } from "@ngrx/effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 import { environment } from "../../environments/environment";
 import { AuthEffects } from "../auth/auth.effects";
@@ -19,7 +20,11 @@ import { coreReducers } from "./core.reducer";
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     EffectsModule.forRoot([AuthEffects]),
-    StoreModule.forRoot(coreReducers)
+    StoreModule.forRoot(coreReducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // number of past states to retain
+      logOnly: environment.production // Disable other features while in prod.
+    })
   ],
   declarations: []
 })
