5c93792f2ee28f9f91c3a1501be716e0e4b0e379
einargs <einargs@gmail.com>
2018-03-27 12:50:04 -0500

    Refactored cloud functions to use expressjs.

---
+++ functions/src/user.ts
@@ -0,0 +1,37 @@
+import * as functions from 'firebase-functions';
+import * as firebaseAdmin from "firebase-admin";
+
+// Initialize firebase admin with environmental configuration information
+firebaseAdmin.initializeApp(functions.config().firebase);
+
+export interface UserData {
+  // Map of device ids to boolean values
+  allowedDevices: {[id: string]: boolean;}
+}
+
+export interface User {
+  uid: string;
+  data: UserData;
+}
+
+// Get the user id from the token and app-specific user data from firestore.
+export async function getUserFromToken(token: string): Promise<User> {
+  const user = await firebaseAdmin.auth().verifyIdToken(token);
+  const userDocumentSnapShot = await firebaseAdmin.firestore()
+      .doc(`users/${user.uid}`)
+      .get();
+  const userData: UserData = userDocumentSnapShot.data() as UserData;
+
+  return {
+    uid: user.uid,
+    data: userData
+  };
+}
+
+export function canUserAccessDevice(user: User, deviceId: string) {
+  return user.data.allowedDevices[deviceId];
+}
+
+export function canUserConfigureDevice(user: User, deviceId: string) {
+  return user.data.allowedDevices[deviceId];
+}
