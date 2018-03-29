b148e9bdb6ab95816bccd6e902374138f9cac1d4
einargs <einargs@gmail.com>
2018-03-29 13:30:10 -0500

    Adjust the way initial device feature state is provided.
    The previous method was causing problems when compiled with AOT; see the
    added comment in device.module.ts for details.

--- src/app/device/device.module.ts
+++ src/app/device/device.module.ts
@@ -11,13 +11,25 @@ import { DeviceListComponent } from './device-list/device-list.component';
 import { DeviceControlComponent } from './device-control/device-control.component';
 import { deviceFeatureReducers, initialDeviceFeatureState } from "./device-feature.reducer";
 
+// Angular Ahead-Of-Time compilation does not support non-statically resolvable
+// tokens inside of decorators. Because I'm using an entity adapter, and the
+// .getInitialState method that goes with that adapter, I have to work around
+// this requirement by providing a function that returns the initial state
+// instead. This is an option supported by NgRx; see the api documentation for
+// the store module for more details:
+// https://github.com/ngrx/platform/blob/master/docs/store/api.md
+// The function also has to be exported for some reason? So yeah...
+export function getInitialDeviceFeatureState() {
+  return initialDeviceFeatureState
+}
+
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
     DeviceRoutingModule,
     StoreModule.forFeature("deviceFeature", deviceFeatureReducers, {
-      initialState: initialDeviceFeatureState
+      initialState: getInitialDeviceFeatureState
     }),
     EffectsModule.forFeature([DeviceEffects])
   ],
